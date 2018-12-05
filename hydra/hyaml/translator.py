aacca262343aca409aea4af9e3429b39484b03f5
Nikita Shilnikov <ns@latera.ru>
2018-12-06 00:06:40 +0300

    HARD-570 Add tests for simple literals

--- hydra/hyaml/translator.py
+++ hydra/hyaml/translator.py
@@ -4,12 +4,24 @@ from hydra.hyaml.grammar import HyamlVisitor, HyamlLexer, HyamlParser
 
 class Visitor(HyamlVisitor):
     def visitExpr(self, ctx: HyamlParser.ExprContext):
-        token_type = ctx.start.type
+        fst = ctx.start
 
-        if token_type == HyamlParser.VAR:
+        if fst.type == HyamlParser.VAR:
             var_name = ctx.VAR().symbol.text[1:]
             expr = "variables.get('%s')" % var_name
             self._buffer.append(expr)
+        elif fst.type == HyamlParser.NUMBER:
+            number = ctx.NUMBER().symbol.text
+            self._buffer.append(number)
+        elif fst.type == HyamlParser.STRING:
+            string = ctx.STRING().symbol.text
+            self._buffer.append(string)
+        elif fst.type == HyamlParser.EMPTY_HASH:
+            self._buffer.append("{}")
+        elif fst.type == HyamlParser.TRUE:
+            self._buffer.append("True")
+        elif fst.type == HyamlParser.FALSE:
+            self._buffer.append("False")
 
         return super().visitExpr(ctx)
 
--- tests/test_basic_grammar.py
+++ tests/test_basic_grammar.py
@@ -8,9 +8,25 @@ class TestBasicGrammar(TestCase):
     def assertTranslated(self, expr, expectation):
         self.assertEqual(translate(expr + "\n"), expectation)
 
-    def test_sample(self):
-        self.assertTranslated("", "")
-
     def test_variable(self):
-        print("fooooo")
         self.assertTranslated("$foo", "variables.get('foo')")
+
+    def test_integer(self):
+        self.assertTranslated("50", "50")
+        self.assertTranslated("-50", "-50")
+
+    def test_float(self):
+        self.assertTranslated("50.01", "50.01")
+        self.assertTranslated("-50.01", "-50.01")
+
+    def test_string(self):
+        self.assertTranslated("'foo'", "'foo'")
+        self.assertTranslated('"foo"', '"foo"')
+
+    def test_empty_hash(self):
+        self.assertTranslated("{}", "{}")
+
+    def test_boolean(self):
+        self.assertTranslated("true", "True")
+        self.assertTranslated("false", "False")
+
