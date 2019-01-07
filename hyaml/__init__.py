3bf266a1d0eeec8f24de39d9953b182f1c6c051f
Nikita Shilnikov <ns@latera.ru>
2019-01-07 22:21:38 +0100

    Replace repl with __main__.py

--- hyaml/__init__.py
+++ hyaml/__init__.py
@@ -1,10 +1,10 @@
 def main():
     from hyaml.translator import translate
-    from hyaml.compiler import compile
+    from hyaml.compiler import compile_expression
     from code import interact
 
     def evaluate(expr, **lcls):
-        fn = compile(expr, bindings=("variables",))
+        fn = compile_expression(expr, bindings=("variables",))
         return fn(variables=lcls)
 
     interact(local={"evaluate": evaluate, "translate": translate})
